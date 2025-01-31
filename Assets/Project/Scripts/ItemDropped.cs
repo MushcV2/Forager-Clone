0cfb01f559640f97b58a0269b6bc38a06c8581bc
MushcV2 <titi.c2006@gmail.com>
2025-01-30 23:01:17 -0300

    Inventory 3.0

--- Assets/Project/Scripts/ItemDropped.cs
+++ Assets/Project/Scripts/ItemDropped.cs
@@ -2,14 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public enum Types
+{
+    Stone,
+    Wood
+}
+
 public class ItemDropped : MonoBehaviour
 {
     public int amount;
+    public Types type;
 
     private void OnCollisionEnter2D(Collision2D _other)
     {
         if (!_other.gameObject.CompareTag("Player")) return;
 
-        _other.gameObject.GetComponent<PlayerInventory>().AddItem(gameObject, amount);
+        _other.gameObject.GetComponent<PlayerInventory>().AddItem(gameObject, amount, type);
     }
 }
--- Assets/Project/Scripts/PlayerInventory.cs
+++ Assets/Project/Scripts/PlayerInventory.cs
@@ -1,31 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-[System.Serializable]
-public class Inventory
-{
-    public GameObject item;
-    public int amount;
-
-    public Inventory(GameObject _item, int _amout)
-    {
-        item = _item;
-        amount = _amout;
-    }
-}
 
 public class PlayerInventory : MonoBehaviour
 {
+    [System.Serializable]
+    public class Inventory
+    {
+        public GameObject item;
+        public int amount;
+        public Types type;
+
+        public Inventory(GameObject _item, int _amount, Types _type)
+        {
+            item = _item;
+            amount = _amount;
+            type = _type;
+        }
+    }
 
     [Header("Inventory")]
     [SerializeField] private int inventorySlots;
+    [SerializeField] private int slotSelected;
     [SerializeField] private GameObject inventoryUI;
     [SerializeField] private GameObject inventorySlotPrefab;
+    [SerializeField] private GameObject slotHighlight;
     [SerializeField] private Image itemImage;
 
     [SerializeField] private List<Inventory> inventoryList = new List<Inventory>();
+    [SerializeField] private GameObject testOBJ;
 
     private void Start()
     {
@@ -34,16 +40,18 @@ public class PlayerInventory : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyUp(KeyCode.F)) AddItem(inventoryUI, 10);
+        if (Input.GetKeyUp(KeyCode.F)) AddItem(testOBJ, 10, Types.Stone);
+
+        ChangeSlotSelected();
     }
 
     private void InitInventory()
     {
-        for (int i = 0; i < inventorySlots; i++)
+        for (int i = 1; i <= inventorySlots; i++)
         {
             GameObject _slot = Instantiate(inventorySlotPrefab);
 
-            _slot.name = $"Slot: {i}";
+            _slot.name = i.ToString();
             _slot.transform.SetParent(inventoryUI.transform, false);
 
             _slot.transform.Find("Image").gameObject.SetActive(false);
@@ -51,7 +59,98 @@ public class PlayerInventory : MonoBehaviour
         }
     }
 
-    private GameObject SelectedInventoryEmpty()
+    private void ChangeSlotSelected()
+    {
+        if (Input.anyKeyDown)
+        {
+            for (int i = 1; i <= inventorySlots; i++)
+            {
+                if (Input.GetKeyDown("" + i))
+                {
+                    slotHighlight.SetActive(true);
+
+                    slotHighlight.transform.SetParent(inventoryUI.transform.Find(i.ToString()), false);
+                    slotHighlight.transform.position = inventoryUI.transform.Find(i.ToString()).position;
+                    slotHighlight.transform.SetAsFirstSibling();
+
+                    slotSelected = i;
+                }
+            }
+        }
+    }
+
+    private GameObject SelectedInventoryEmpty(GameObject _itemToVerify)
+    {
+        GameObject _slot = null;
+
+        for (int i = 0; i < inventoryUI.transform.childCount; i++)
+        {
+            if (inventoryUI.transform.GetChild(i).GetComponent<InventorySlot>().haveItem)
+            {
+                var _onList = inventoryList.Find(_list => _list.item == _itemToVerify);
+                if (_onList != null)
+                {
+                    _slot = inventoryUI.transform.GetChild(i).gameObject;
+                    break;
+                }
+            }
+            else _slot = inventoryUI.transform.GetChild(i).gameObject;
+        }
+
+        return _slot;
+    }
+
+    public void AddItem(GameObject _obj, int _amount, Types _type)
+    {
+        GameObject _slot = SelectedInventoryEmpty(_obj);
+
+        if (_slot == null)
+        {
+            Debug.Log("Inventario cheio");
+            return;
+        }
+
+        Image _slotImage = _slot.transform.Find("Image").GetComponent<Image>();
+        TextMeshProUGUI _slotText = _slot.transform.Find("Amount").GetComponent<TextMeshProUGUI>();
+
+        _slotImage.gameObject.SetActive(true);
+        _slotText.gameObject.SetActive(true);
+
+        var _onList = inventoryList.Find(_list => _list.item == _obj);
+        if (_onList != null)
+        {
+            _onList.amount += _amount;
+            _slotText.text = _onList.amount.ToString();
+
+            Debug.Log("Item stackado");
+        }
+        else
+        {
+            _slot.GetComponent<InventorySlot>().haveItem = true;
+
+            _slotText.text = _amount.ToString();
+            _slotImage.sprite = _obj.GetComponent<SpriteRenderer>().sprite;
+
+            inventoryList.Add(new Inventory(_obj, _amount, _type));
+
+            Debug.Log("Item adicionado");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private GameObject NAOPERDER()
     {
         GameObject _selectedSlot = null;
 
@@ -67,19 +166,5 @@ public class PlayerInventory : MonoBehaviour
         }
 
         return _selectedSlot;
-    }
-
-    public void AddItem(GameObject _obj, int _amount)
-    {
-        GameObject _slot = SelectedInventoryEmpty();
-
-        if (_slot == null)
-        {
-            Debug.Log("Inventario lotado");
-            return;
-        }
-
-        inventoryList.Add(new Inventory(_obj, _amount));
-        _slot.GetComponent<InventorySlot>().haveItem = true;
     }
 }
